7a7de259f3ea692c291cdf0158308cb9d5363ab7
Flavio <flaviopagano1@gmail.com>
2023-03-15 16:55:30 +0100

    Created 2 more methods to find all Users, and by Id; to be implemented in User entity

--- src/main/java/co/develope/SpringSocialNetwork/repositories/UserRepository.java
+++ src/main/java/co/develope/SpringSocialNetwork/repositories/UserRepository.java
@@ -4,12 +4,18 @@ import co.develope.SpringSocialNetwork.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 
 public interface UserRepository extends JpaRepository <User, String> {
 
+    List<User> findAll();
+
     User findUser(String name);
+
+    User findById(int id);
 
     User findByEmail(String email);
 
