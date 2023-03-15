11eab96e73e59c8dc30150c5ac0296a38fed6e04
Aldo Mancuso <mancusoaldo8 @gmail.com>
2023-03-15 18:47:34 +0100

    update constructor all param

--- src/main/java/co/develope/SpringSocialNetwork/entities/User.java
+++ src/main/java/co/develope/SpringSocialNetwork/entities/User.java
@@ -20,6 +20,15 @@ public class User {
 
     }
 
+    public User(int id, String name, String surname, String nickname, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
+
     public User(int id, String name, String surname) {
         this.id = id;
         this.name = name;
