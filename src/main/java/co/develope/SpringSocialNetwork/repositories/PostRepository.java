1a91a0a7dd1bc5102ee8ce5ac07b954aed636212
Alma Caciula Negrea <almacaciulanegrea@gmail.com>
2023-03-19 16:29:27 +0100

    Merge di develop-database in develop

--- src/main/java/co/develope/SpringSocialNetwork/repositories/PostRepository.java
+++ src/main/java/co/develope/SpringSocialNetwork/repositories/PostRepository.java
@@ -2,6 +2,8 @@ package co.develope.SpringSocialNetwork.repositories;
 
 import co.develope.SpringSocialNetwork.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post,Integer> {
 }
